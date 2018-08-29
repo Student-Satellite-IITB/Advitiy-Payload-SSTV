70188dcc8e1cfc3939d6cfc1896f703c8da7397c
PuneetShrivas <puneetshrivas32@gmail.com>
2018-08-29 18:57:30 +0530

    Added ATXmega_SSTV_Encode Folder,
    SPI, USART, Interrupt timer working, Waveform generator running

---
+++ ATXmega_SSTV_Encode/ATXmega_SSTV_Encode/Debug/makedep.mk
@@ -0,0 +1,16 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+main.cpp
+
+pmic_driver.c
+
+sample code.c
+
+spi_driver.c
+
+TC_driver.c
+
+usart.c
+
---
+++ ATXmega_SSTV_Encode/ATXmega_SSTV_Encode/sample code.c
@@ -0,0 +1,6 @@
+/*
+ * sample_code.c
+ *
+ * Created: 22-08-2018 15:04:24
+ *  Author: Puneet Shrivas
+ */ 
